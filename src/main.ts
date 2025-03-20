2496bf324ea673f77720d46e8def65bf5d3809bf
David <david.leon.m@gmail.com>
2025-03-20 09:25:30 +0100

    https

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app.module'
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino'
 import { GlobalConfig } from './config/global.config'
+import fs from 'node:fs'
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface'
 
 async function bootstrap(): Promise<void> {
+  const httpsOptions: HttpsOptions = GlobalConfig.environment.isProduction
+    ? {
+      key: fs.readFileSync(GlobalConfig.server.keyFile),
+      cert: fs.readFileSync(GlobalConfig.server.certFile),
+    }
+    : undefined
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    httpsOptions: httpsOptions,
   })
 
   app.enableShutdownHooks().useGlobalInterceptors(new LoggerErrorInterceptor()).useLogger(app.get(Logger))
